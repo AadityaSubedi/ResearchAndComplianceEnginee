f375b10f2b61860196f0eb709e5a352154421fd2
Aaditya Subedi <aaditya.subedi@docsumo.com>
2026-02-14 16:11:22 +0545

    add role in the user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,18 @@
 from app.models.base import BaseDocument
+from enum import Enum
+
+class RolesEnum(str, Enum): 
+    """Roles enum"""
+    OWNER = "owner"
+    ADMIN = "admin"
+    MEMBER = "member"
 
 
 class User(BaseDocument):  # pylint: disable=too-many-ancestors
     """User schema"""
     email: str
     full_name: str
+    role: RolesEnum
 
     class Settings:
         """Settings"""
